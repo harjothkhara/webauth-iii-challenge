6afa18bf9f4ce7fc5316712e8eb2792ef457ef2a
harjothkhara <harjoth.khara@gmail.com>
2019-06-12 15:51:53 -0700

    added dotenv dependency, register end point complete

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../data/helpers/user-model.js');
+
+router.post('/register', async (req, res) => {
+    let { username, password, department } = req.body;
+    if (!username || !password || !department) {
+        res.status(401).json({ message: "Please enter valid credentials" });
+    } else {
+        password = bcrypt.hashSync(password, 8);
+        try {
+            const newUser = await db.create({ username, password, department });
+            if (newUser) {
+                res.status(201).json(newUser);
+            }
+        } catch(error) {
+            res.status(500).json({ message: `Your user could not be created ${error}` });
+        }
+    }
+});
+
 
 module.exports = router;
