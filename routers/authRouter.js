4ccb8bfb8cf01bf9f8776e9c3cb71b567f82b877
harjothkhara <harjoth.khara@gmail.com>
2019-06-12 16:10:23 -0700

    .env file created for secret, jwt imported to authRouter, userRouter and restricted middleware created

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -20,5 +20,26 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    const{ username, password } = req.body;
+    if (!username || !password) {
+        res.status(401).json({ message: "Please enter valid credentials." });
+    } else {
+        try {
+            const user = await db.findByUser(username);
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome ${username}` });
+            } else {
+                res.status(401).json({ message: "You shall not pass!" });
+            }
+        } catch (error) {
+            res.status(500).json({ message: `Login failed ${error}` });
+        }
+    }
+});
+
+router.get('/logout', (req, res) => {
+    
+});
 
 module.exports = router;
