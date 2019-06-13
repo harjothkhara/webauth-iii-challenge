cf3e4ea3993df9d9e57f2880dcef81dffda938a9
harjothkhara <harjoth.khara@gmail.com>
2019-06-13 13:59:26 -0700

    added submit form, axios request, saved token to localstorage

--- webauth/src/auth/Login.js
+++ webauth/src/auth/Login.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
-    // state = {  }
+    state = { 
+        username: 'luke',
+        password: 'skywalker'
+     };
+
+     handleChange = e => {
+        const { id, value } = e.target;
+
+        this.setState({ [id]: value });
+     };
+
+     SubmitForm = e => {
+         e.preventDefault();
+         const endpoint = 'http://localhost:5000/api/auth/login';
+
+         axios
+            .post(endpoint, this.state)
+            .then(res => {
+                localStorage.setItem('jwt', res.data.token)
+            })
+              .catch(err => {
+                  console.log('Login Error', err)
+              });
+     };
+
     render() { 
-        return <h3> Login Component </h3>
-    }
+        return (
+        <>
+        <h2> Login </h2>
+        <form onSubmit={this.SubmitForm} >
+            <div>
+                <label htmlFor='username' />
+                    <input
+                        id='username'
+                        onChange={this.handleChange}
+                        value={this.state.username}
+                        type='text'
+                    />
+            </div>
+            <div>
+                <label htmlFor='password' />
+                    <input
+                        id='password'
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                        type='password'
+                    />
+            </div>
+            <div>
+                <button type='submit'>Login</button>
+            </div>
+        </form>
+        </>
+    );
+  }
 }
+
+
  
 export default Login;
